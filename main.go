74daf969df84f6c22c609c11bd0221d656480b47
Favour Ori <orifavour2000@gmail.com>
2020-01-02 19:39:10 +0200

    functions

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
+
+	"github.com/favourori/go_intro/stringUtils"
 )
+
+func greet(name string) string {
+	return "Hello, " + name
+
+}
 
 func main() {
 	//fmt.Println("Hello Go!")
@@ -12,7 +20,12 @@ func main() {
 	var age int = 23
 	const isloggedIn = false
 	fmt.Println(name, age, isloggedIn)
+	fmt.Println(math.Floor(20.6))
+	fmt.Println(stringUtils.Reverse("Hello"))
 
 	//print data type
 	fmt.Printf("%T\n", age)
+
+	//functions
+	fmt.Println(greet("David"))
 }
